85f77407adb09ad1ef28be580281812626a28f79
KolemanPa <46792228+KolemanPa@users.noreply.github.com>
2019-02-25 12:26:52 -0600

    Update OneAndZero.java
    added comments

--- 10by10/src/OneAndZero.java
+++ 10by10/src/OneAndZero.java
@@ -1,4 +1,9 @@
 
+/**
+* Program:             10x10
+* Developer:           Koleman parsley
+* Problem:             Create a 10 by 10 grid with either 1's or 0's
+*/
 import java.util.Random;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -14,27 +19,22 @@ import javafx.stage.Stage;
  */
 public class OneAndZero extends Application{
 
-
-
-
+        // Sets the stage and size
         @Override
         public void start(Stage primaryStage) {
-
             int size = 10;
             int length = size;
             int width = size;
-
+                //if you want to make grid bigger change the size variable
             GridPane pane = new GridPane();    
-
+                // for loop to create each box
             for(int y = 0; y < length; y++){
                 for(int x = 0; x < width; x++){
-
                     Random random = new Random();
                     int rando1 = random.nextInt(2);
 
-                    
-                    
-                    
+                  
+                        
                     // Creating the textfields!!
                     TextField tf = new TextField();
                     tf.setPrefHeight(50);
@@ -46,13 +46,14 @@ public class OneAndZero extends Application{
                     pane.getChildren().add(tf);
                 }
             }
+                // SHows the grids, change it to false to hide them
             pane.setGridLinesVisible(true);   
             Scene scene = new Scene(pane, 700, 700);    
             primaryStage.setTitle("10 by 10 thingy");
             primaryStage.setScene(scene);
             primaryStage.show();
         }
-
+        // show the program
         public static void main(String[] args) {    
             launch(args);
         }    
